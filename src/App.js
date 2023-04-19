19f9097296a12af1229a7dc980c992cddc6f5c59
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-04-19 15:00:38 +0300

    testing conflict

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-//import logo from './logo.svg';
+
 import './App.css';
 
 import Comments from './components/Comments'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-       
+      
+        <Routes>
         <Comments />
+        </Routes>
+        
     </div>
   );
 }
